c4b242ed24bd4848ea250ee5861e12dea8e94f5d
Lars-Erik <lea@markedspartner.no>
2021-12-05 17:52:45 +0100

    Day 5 solved

--- day5/Hypothermal_Vents.cs
+++ day5/Hypothermal_Vents.cs
@@ -18,15 +18,34 @@ namespace day5
             var allSegments = Parse(inputData);
             var segments = allSegments.Where(x => x.IsStraight).ToArray();
 
+            var overlaps = CountOverlaps(draw, segments);
+
+            Assert.AreEqual(expected, overlaps);
+        }
+
+        [TestCase("day5.sample.txt", 12, true)]
+        [TestCase("day5.input.txt", 21104, true, Category = "Slow")]
+        public void Are_Best_Avoided_When_Overlapping_Including_Diagonal(string input, int expected, bool draw)
+        {
+            var inputData = Resources.GetResourceLines(GetType(), input);
+            var allSegments = Parse(inputData);
+            
+            var overlaps = CountOverlaps(draw, allSegments.ToArray());
+
+            Assert.AreEqual(expected, overlaps);
+        }
+
+        private static int CountOverlaps(bool draw, Line[] segments)
+        {
             var overlaps = 0;
 
             var area = (
                 X1: segments.Min(x => x.MinX),
                 Y1: segments.Min(x => x.MinY),
-                X2: segments.Max(x => x.MaxX), 
-                Y2: segments.Max(x => x.MaxY) 
+                X2: segments.Max(x => x.MaxX),
+                Y2: segments.Max(x => x.MaxY)
             );
-            
+
             for (var y = area.Y1; y <= area.Y2; y++)
             {
                 for (var x = area.X1; x <= area.X2; x++)
@@ -41,7 +60,7 @@ namespace day5
                     }
 
                     if (draw)
-                    { 
+                    {
                         if (count > 0)
                         {
                             Console.Write(count);
@@ -54,12 +73,29 @@ namespace day5
                 }
 
                 if (draw)
-                { 
+                {
                     Console.Write(Environment.NewLine);
                 }
             }
 
-            Assert.AreEqual(expected, overlaps);
+            return overlaps;
+        }
+
+        [TestCase(9, 7, 7, 9, 8, 8, true)]
+        [TestCase(7, 9, 9, 7, 8, 8, true)]
+        [TestCase(2, 5, 5, 8, 3, 6, true)]
+        [TestCase(2, 5, 5, 8, 4, 6, false)]
+        [TestCase(0, 0, 0, -10, 0, -5, true)]
+        [TestCase(0, 0, -10, 0, -3, 0, true)]
+        [TestCase(0, 0, 10, 0, 3, 0, true)]
+        [TestCase(0, 0, 10, 0, -3, 0, false)]
+        public void Can_Intersect_Diagonally(int x1, int y1, int x2, int y2, int px, int py, bool expected)
+        {
+            var xy1 = new Point(x1, y1);
+            var xy2 = new Point(x2, y2);
+            var line = new Line(xy1, xy2);
+
+            Assert.AreEqual(expected, line.Intersects(new Point(px, py)));
         }
 
         private Area Parse(string[] inputData)
@@ -94,18 +130,54 @@ namespace day5
 
         record Point(int X, int Y);
 
-        record Line(Point From, Point To)
+        record Line
         {
-            public bool IsStraight => From.X == To.X || From.Y == To.Y;
-            public int MinX => Math.Min(From.X, To.X);
-            public int MaxX => Math.Max(From.X, To.X);
-            public int MinY => Math.Min(From.Y, To.Y);
-            public int MaxY => Math.Max(From.Y, To.Y);
+            public readonly Point From;
+            public readonly Point To;
+
+            public readonly bool IsStraight;
+            public readonly int MinX;
+            public readonly int MaxX;
+            public readonly int MinY;
+            public readonly int MaxY;
+
+            public readonly int Slope;
+            public readonly int YIntercept;
+
+            public Line(Point from, Point to)
+            {
+                From = from;
+                To = to;
+
+                IsStraight = From.X == To.X || From.Y == To.Y;
+                MinX = Math.Min(From.X, To.X);
+                MaxX = Math.Max(From.X, To.X);
+                MinY = Math.Min(From.Y, To.Y);
+                MaxY = Math.Max(From.Y, To.Y);
+
+                if (To.X - From.X != 0)
+                { 
+                    Slope = (To.Y - From.Y) / (To.X - From.X);
+                    YIntercept = (From.X * Slope - From.Y) * -1;
+                }
+            }
 
             public bool Intersects(Point point)
             {
-                return (point.X >= MinX && point.X <= MaxX && (point.Y == MinY || point.Y == MaxY)) 
-                    || (point.Y >= MinY && point.Y <= MaxY && (point.X == MinX || point.X == MaxX));
+                var withinY = point.Y >= MinY && point.Y <= MaxY;
+                if (To.X - From.X == 0)
+                {
+                    return point.X == From.X && withinY;
+                }
+
+                var withinX = point.X >= MinX && point.X <= MaxX;
+                if (To.Y - From.Y == 0)
+                {
+                    return point.Y == From.Y && withinX;
+                }
+
+                return (point.X * Slope + YIntercept) == point.Y
+                    && withinX && withinY;
             }
         };
     }
