a61965b055d46f036a8a8c379383a89d921dd7e2
Lars-Erik <lea@markedspartner.no>
2021-12-11 15:54:05 +0100

    Day 10 solved (late commit)

--- day10/Parsing_Navigation_Syntax.cs
+++ day10/Parsing_Navigation_Syntax.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Text;
 using common;
 using NUnit.Framework;
 
@@ -11,29 +12,32 @@ namespace day10
     [TestFixture]
     public class Parsing_Navigation_Syntax
     {
+        private NavTokenizer tokenizer;
+        private NavParser parser;
+
         [SetUp]
         public void Setup()
         {
+            tokenizer = new NavTokenizer();
+            parser = new NavParser();
         }
 
         [Test]
         [TestCase("day10.sample.txt", 26397)]
-        [TestCase("day10.input.txt", 0)]
+        [TestCase("day10.input.txt", 167379)]
         public void Finds_Invalid_Closing_Brackets(string resource, int expectedScore)
         {
-            var lines = Resources.GetResourceLines(typeof(Parsing_Navigation_Syntax), resource);
+            var lines = Resources.GetResourceLines(typeof(Parsing_Navigation_Syntax), TestContext.CurrentContext.Test.Arguments[0]!.ToString());
             var exceptions = new List<ParsingException>();
 
             foreach (var line in lines)
             {
                 Console.Write(line);
 
-                var tokenizer = new NavTokenizer();
                 var tokens = tokenizer.Tokenize(line);
-                var parser = new NavParser();
                 try
                 {
-                    parser.Parse(tokens);
+                    var ast = parser.Parse(tokens);
                     Console.WriteLine(" - Inconclusive");
                 }
                 catch (ParsingException ex)
@@ -54,6 +58,93 @@ namespace day10
             var score = exceptions.Sum(x => pointsTable[x.Token]);
 
             Assert.AreEqual(expectedScore, score);
+        }
+
+        [Test]
+        [TestCase("day10.sample.txt", 288957)]
+        [TestCase("day10.input.txt", 2776842859)]
+        public void AutoCompletes_Invalid_Lines(string resource, long expectedScore)
+        {
+            var lines = Resources.GetResourceLines(typeof(Parsing_Navigation_Syntax), TestContext.CurrentContext.Test.Arguments[0]!.ToString());
+            var incomplete = new List<Ast>();
+            foreach (var line in lines)
+            {
+                var tokens = tokenizer.Tokenize(line);
+                try
+                {
+                    var lineAst = parser.Parse(tokens);
+                    incomplete.Add(lineAst);
+                }
+                catch (GroupCloseException ex)
+                {
+                    // We ignore the invalid lines
+                }
+            }
+
+            var scoreTable = new Dictionary<NavToken, int>
+            {
+                {NavTokens.RParen, 1},
+                {NavTokens.RSquare, 2},
+                {NavTokens.RCurl, 3},
+                {NavTokens.GT, 4},
+            };
+
+            List<long> sums = new List<long>();
+            foreach(var ast in incomplete)
+            { 
+                var autocompleteTokens = new List<NavToken>();
+                AutoComplete(ast.Nodes, autocompleteTokens);
+                var sum = autocompleteTokens.Aggregate((long)0, (prev, token) => prev * 5 + scoreTable[token]);
+                sums.Add(sum);
+
+                Console.Write(String.Join("", autocompleteTokens));
+                Console.Write(" - ");
+                Console.WriteLine(sum);
+            }
+
+            sums = sums.OrderBy(x => x).ToList();
+            Console.WriteLine(String.Join("\r\n", sums));
+            var winner = sums[(sums.Count - 1) / 2];
+            Assert.AreEqual(expectedScore, winner);
+        }
+
+        public void AutoComplete(IEnumerable<Node> nodes, List<NavToken> collectTo)
+        {
+            foreach (var node in nodes)
+            {
+                if (node is NodeGroup group)
+                {
+                    AutoComplete(node.Nodes, collectTo);
+                    if (!group.IsClosed)
+                    {
+                        collectTo.Add(NavParser.EndTokensByStartToken[group.StartToken]);
+                    }
+                }
+                else
+                {
+                    throw new Exception("Don't know how to autocomplete this node yet");
+                }
+            }
+        }
+
+        public void PrintNodes(StringBuilder builder, IEnumerable<Node> nodes, int level)
+        {
+            var pad = new string(' ', level * 2);
+            foreach (var node in nodes)
+            {
+                NavToken startMessage;
+                if (node is NodeGroup group)
+                { 
+                    builder.AppendLine($"{pad}{group.StartToken}");
+                    PrintNodes(builder, node.Nodes, level + 1);
+                    var endToken = group.IsClosed ? group.EndToken!.Value.Character : '~';
+                    builder.AppendLine($"{pad}{endToken}");
+                }
+                else
+                { 
+                    throw new Exception("Don't know how to write this node yet");
+                }
+            }
         }
 
         [Test]
@@ -94,6 +185,22 @@ namespace day10
         private Node curNode;
         private NavToken curToken;
 
+        public static readonly Dictionary<NavToken, NavToken> StartTokensByEndToken = new Dictionary<NavToken, NavToken>
+        {
+            {NavTokens.RParen, NavTokens.LParen},
+            {NavTokens.RCurl, NavTokens.LCurl},
+            {NavTokens.RSquare, NavTokens.LSquare},
+            {NavTokens.GT, NavTokens.LT},
+        };
+
+        public static readonly Dictionary<NavToken, NavToken> EndTokensByStartToken = new Dictionary<NavToken, NavToken>
+        {
+            {NavTokens.LParen, NavTokens.RParen},
+            {NavTokens.LCurl, NavTokens.RCurl},
+            {NavTokens.LSquare, NavTokens.RSquare},
+            {NavTokens.LT, NavTokens.GT},
+        };
+
         public Ast Parse(NavToken[] tokens)
         {
             index = 0;
@@ -128,29 +235,13 @@ namespace day10
 
         private void EndGroup()
         {
-            Dictionary<NavToken, NavToken> expectedStarts = new Dictionary<NavToken, NavToken>
-            {
-                {NavTokens.RParen, NavTokens.LParen},
-                {NavTokens.RCurl, NavTokens.LCurl},
-                {NavTokens.RSquare, NavTokens.LSquare},
-                {NavTokens.GT, NavTokens.LT},
-            };
-
-            Dictionary<NavToken, NavToken> expectedEnds = new Dictionary<NavToken, NavToken>
-            {
-                {NavTokens.LParen, NavTokens.RParen},
-                {NavTokens.LCurl, NavTokens.RCurl},
-                {NavTokens.LSquare, NavTokens.RSquare},
-                {NavTokens.LT, NavTokens.GT},
-            };
-
             var curGroup = curNode as NodeGroup;
             if (curGroup == null) throw new Exception("No group to close");
 
-            var expectedStartToken = expectedStarts[curToken];
+            var expectedStartToken = StartTokensByEndToken[curToken];
             if (!curGroup.StartToken.Equals(expectedStartToken))
             {
-                throw new GroupCloseException(curToken, index, expectedEnds[curGroup.StartToken]);
+                throw new GroupCloseException(curToken, index, EndTokensByStartToken[curGroup.StartToken]);
             }
 
             if (curNode.Parent == null)
@@ -158,6 +249,7 @@ namespace day10
                 throw new Exception("Unexpected token. Expected EOF.");
             }
 
+            curGroup.Close(curToken);
             curNode = curNode.Parent;
         }
     }
@@ -222,9 +314,12 @@ namespace day10
     public class NodeGroup : Node
     {
         private readonly NavToken startToken;
+        private NavToken? endToken;
         private readonly List<Node> nodes;
 
         public NavToken StartToken => startToken;
+        public NavToken? EndToken => endToken;
+        public bool IsClosed => endToken.HasValue;
 
         public NodeGroup(Node parent, NavToken startToken) : base(parent)
         {
@@ -234,7 +329,17 @@ namespace day10
 
         public override void Add(Node child)
         {
+            if (IsClosed)
+            {
+                throw new Exception("Group is already closed");
+            }
+            
             nodes.Add(child);
+        }
+
+        public void Close(NavToken token)
+        {
+            endToken = token;
         }
     }
 
