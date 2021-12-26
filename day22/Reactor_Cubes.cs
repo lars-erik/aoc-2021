aaaba2dbfb657e4193c54e599bcfd158e0823695
Lars-Erik <lea@markedspartner.no>
2021-12-26 10:56:11 +0100

    Didn't manage

--- day22/Reactor_Cubes.cs
+++ day22/Reactor_Cubes.cs
@@ -1,8 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using common;
-using Csg;
 using NUnit.Framework;
 using Resources = common.Resources;
 using Vector3 = System.Numerics.Vector3;
@@ -22,22 +22,7 @@ namespace day22
         //[TestCase("LargerSample", 590784)]
         public void Are_Toggled(string resource, long expectedCubes)
         {
-            var lines = Resources.GetResourceLines(typeof(Reactor_Cubes), $"day22.{resource}.txt");
-            var regions = new List<Region>();
-            foreach (var line in lines)
-            {
-                var mainParts = line.Split(" ");
-                var state = mainParts[0];
-                var lineRanges = mainParts[1].Split(",").Select(r =>
-                {
-                    var axisParts = r.Split("=");
-                    var axis = axisParts[0];
-                    var range = axisParts[1].Split("..").Select(int.Parse).OrderBy(x => x).ToArray();
-                    return (axis, range:(min:range[0], max:range[1]));
-                })
-                    .ToDictionary(x => x.axis, x => x.range);
-                regions.Add(new Region(state, lineRanges));
-            }
+            var regions = CreateRegions(resource);
 
             var newRegions = new List<Region>();
             for (var i = 0; i < regions.Count; i++)
@@ -62,9 +47,9 @@ namespace day22
                 {
                     var intersecting = newRegions.First();
 
-                    var newRegion = intersecting.Add(region);
+                    var result = intersecting.Intersect(region);
                     newRegions.Remove(intersecting);
-                    newRegions.Add(newRegion);
+                    newRegions.AddRange(result);
                 }
                 else if (region.State == "on")
                 {
@@ -89,80 +74,254 @@ namespace day22
         }
 
         [Test]
-        public void This_Csg_Lib_Works()
+        public void Creates_Paths_Along_X()
         {
-            var cubeA = Solids.Cube(size:new Vector3D(2, 1, 1), center: new Vector3D(1, .5, .5));
+            var a = new Region(0, 0, 0, 1, 0, 0);
+            var b = new Region(1, 0, 0, 1, 0, 0);
+            var c = new Region(1, 0, 0, 2, 0, 0);
+            var d = new Region(2, 0, 0, 3, 0, 0);
 
             Console.WriteLine("A");
-            foreach (var polygon in cubeA.Polygons)
-            {
-                Console.WriteLine(polygon.BoundingBox.Min + ", " + polygon.BoundingBox.Max);
-            }
-            Console.WriteLine();
-            
-            var cubeB = Solids.Cube(size:new Vector3D(1, 1, 1), center:new Vector3D(1.5, 0.5, 0.5));
-
-            Console.WriteLine("B");
-            foreach (var polygon in cubeB.Polygons)
-            {
-                Console.WriteLine(polygon.BoundingBox.Min + ", " + polygon.BoundingBox.Max);
-            }
-            Console.WriteLine();
-
-            var cubeC = cubeA.Intersect(cubeB);
-            Console.WriteLine("C");
-            var vertices = cubeC.Polygons.SelectMany(p => p.Vertices.Select(v => v.Pos)).Distinct();
-            foreach (var vert in vertices)
-            {
-                Console.WriteLine(vert);
-            }
-            Console.WriteLine();
+            a.XPaths(a).Select(x => x.ToString()).Dump();
+            Console.WriteLine("AB");
+            a.XPaths(b).Select(x => x.ToString()).Dump();
+            Console.WriteLine("AC");
+            a.XPaths(c).Select(x => x.ToString()).Dump();
+            Console.WriteLine("AD");
+            a.XPaths(d).Select(x => x.ToString()).Dump();
+            Console.WriteLine("BC");
+            b.XPaths(c).Select(x => x.ToString()).Dump();
         }
 
         [Test]
-        public void Size_Of_Intersection_Of_Separate_Is_Zero()
+        public void Creates_New_Boxes_When_Intersecting_Inside()
         {
-            var cubeA = Solids.Cube(new Vector3D(1, 1, 1), new Vector3D(0, 0, 0));
-            var cubeB = Solids.Cube(new Vector3D(1, 1, 1), new Vector3D(10, 0, 0));
-            var intersect = cubeA.Intersect(cubeB);
-            Assert.AreEqual(0, intersect.Polygons.Count);
+            var a = new Region(0, 0, 0, 1, 0, 0);
+            var b = new Region(1, 0, 0, 1, 0, 0);
+            var newBoxes = a.Intersect(b);
+            newBoxes.Select(x => x.ToString()).Dump();
+            Assert.Fail();
+        }
+
+        [Test]
+        public void Creates_New_Boxes_When_Overlapping()
+        {
+            var a = new Region(0, 0, 0, 1, 0, 0);
+            var b = new Region(1, 0, 0, 2, 0, 0);
+            var newBoxes = a.Intersect(b);
+            newBoxes.Select(x => x.ToString()).Dump();
+            Assert.Fail();
+        }
+
+        [Test]
+        public void Creates_New_Boxes_When_Overlapping_2D()
+        {
+            var a = new Region(0, 0, 0, 1, 1, 0);
+            var b = new Region(1, 2, 0, 2, 2, 0);
+            var newBoxes = a.Intersect(b);
+            newBoxes.Select(x => x.ToString()).Dump();
+            Assert.AreEqual(7, newBoxes.Length);
+        }
+
+        [Test]
+        public void Creates_Vertices_For_Sample_Step_1()
+        {
+            var a = new Region(10, 10, 10, 12, 12, 12);
+            var b = new Region(11, 11, 11, 13, 13, 13);
+
+            a.XPaths(b).Select(v => v.ToString()).Dump();
+            a.YPaths(b).Select(v => v.ToString()).Dump();
+            a.ZPaths(b).Select(v => v.ToString()).Dump();
+        }
+
+        [Test]
+        public void Creates_New_Boxes_For_Sample_Step_1()
+        {
+            var a = new Region(10, 10, 10, 12, 12, 12);
+            var b = new Region(11, 11, 11, 13, 13, 13);
+
+            var newBoxes = a.Intersect(b);
+
+            newBoxes.Select(x => x.ToString()).Dump();
+            Console.WriteLine(newBoxes.Length);
+            Assert.Fail();
+        }
+
+        [Test]
+        public void Doesnt_Intersect()
+        {
+            var a = new Region(0, 0, 0, 3, 2, 2);
+            var b = new Region(3, 1, 1, 4, 3, 3);
+            var intersects = a.Intersects(b) && b.Intersects(a);
+            Assert.AreEqual(false, intersects);
+        }
+
+        [Test]
+        public void Instersected_Regions_Create_New_Split_Regions()
+        {
+            var a = new Region(0, 0, 0, 2, 1, 1);
+            var b = new Region(1, 0, 0, 3, 1, 1);
+            var intersect = a.Intersect(b);
+            Assert.AreEqual(3, intersect.Count());
+        }
+
+        private static List<Region> CreateRegions(string resource)
+        {
+            var lines = Resources.GetResourceLines(typeof(Reactor_Cubes), $"day22.{resource}.txt");
+            var regions = new List<Region>();
+            foreach (var line in lines)
+            {
+                var mainParts = line.Split(" ");
+                var state = mainParts[0];
+                var lineRanges = mainParts[1].Split(",").Select(r =>
+                    {
+                        var axisParts = r.Split("=");
+                        var axis = axisParts[0];
+                        var range = axisParts[1].Split("..").Select(int.Parse).OrderBy(x => x).ToArray();
+                        return (axis, range: (min: range[0], max: range[1]));
+                    })
+                    .ToDictionary(x => x.axis, x => x.range);
+                regions.Add(new Region(lineRanges){State = state});
+            }
+
+            return regions;
         }
     }
 
     public class Region
     {
-        private Solid solid;
-        public string State { get; }
+        private float minX;
+        private float maxX;
+        private float minY;
+        private float maxY;
+        private float minZ;
+        private float maxZ;
+        private Vector3 size;
+        private Vector3 center;
+        public string State { get; set; }
 
-        public Region(string state, Dictionary<string, (int min, int max)> axis)
+        public Region(float minX, float minY, float minZ, float maxX, float maxY, float maxZ)
         {
-            State = state;
-
-            double minX = axis["x"].min - .5;
-            double maxX = axis["x"].max + .5;
-            double minY = axis["y"].min - .5;
-            double maxY = axis["y"].max + .5;
-            double minZ = axis["z"].min - .5;
-            double maxZ = axis["z"].max + .5;
-            var size = new Vector3D(Math.Abs(maxX - minX), Math.Abs(maxY - minY), Math.Abs(maxZ - minZ));
-            var center = new Vector3D(minX + (maxX - minX) / 2, minY + (maxY - minY) / 2, minZ + (maxZ - minZ) / 2);
-            solid = Solids.Cube(size, center);
+            this.minX = minX - .5f;
+            this.minY = minY - .5f;
+            this.minZ = minZ - .5f;
+            this.maxX = maxX + .5f;
+            this.maxY = maxY + .5f;
+            this.maxZ = maxZ + .5f;
+            size = new Vector3(MathF.Abs(maxX - minX) + 1, MathF.Abs(maxY - minY) + 1, MathF.Abs(maxZ - minZ) + 1);
+            center = new Vector3(minX + size.X / 2, minY + size.Y / 2, minZ + size.Z / 2);
         }
 
-        public Region(string state, Solid solid)
+        public Region(Dictionary<string, (int min, int max)> axis)
+            : this(axis["x"].min, axis["y"].min, axis["z"].min, axis["x"].max, axis["y"].max, axis["z"].max)
         {
-            State = state;
-            this.solid = solid;
         }
 
         public bool Intersects(Region other)
         {
-            return solid.Intersect(other.solid).Polygons.Count > 0;
+            return minX > other.minX && minX < other.maxX
+                && minY > other.minY && minY < other.maxY
+                && minZ > other.minZ && minZ < other.maxZ
+                || maxX > other.minX && maxX < other.maxX
+                && maxY > other.minY && maxY < other.maxY
+                && maxZ > other.minZ && maxZ < other.maxZ;
         }
 
-        public Region Add(Region other)
+        public Region[] Intersect(Region other)
         {
-            return new Region(State, solid.Union(other.solid));
+            var yPaths = YPaths(other);
+            var zPaths = ZPaths(other);
+            var xPaths = XPaths(other);
+            List<Region> newRegions = new List<Region>();
+            foreach(var xPath in xPaths)
+            {
+                SplitX(newRegions, xPath);
+                other.SplitX(newRegions, xPath);
+            }
+
+            foreach (var yPath in yPaths)
+            {
+                
+            }
+            //foreach (var yPath in yPaths)
+            //{
+            //    foreach (var zPath in zPaths)
+            //    {
+            //        var newRegion = new Region(xPath.X + .5f, yPath.X + .5f, zPath.X + .5f, xPath.Y - .5f, yPath.Y - .5f, zPath.Y - .5f);
+            //        newRegions.Add(newRegion);
+            //    }
+            //}
+
+            return newRegions.Distinct().ToArray();
+        }
+
+        private void SplitX(List<Region> newRegions, Vector2 xPath)
+        {
+            if (xPath.X >= minX && xPath.Y <= maxX)
+            {
+                newRegions.Add(new Region(xPath.X, minY, minZ, xPath.Y, maxY, maxZ));
+            }
+        }
+
+        public Vector2[] XPaths(Region other)
+        {
+            return Paths(minX, maxX, other.minX, other.maxX);
+        }
+
+        public Vector2[] YPaths(Region other)
+        {
+            return Paths(minY, maxY, other.minY, other.maxY);
+        }
+
+        public Vector2[] ZPaths(Region other)
+        {
+            return Paths(minZ, maxZ, other.minZ, other.maxZ);
+        }
+
+        private static Vector2[] Paths(float min, float max, float otherMin, float otherMax)
+        {
+            if (otherMin >= max || min >= otherMax)
+            {
+                return new[]
+                {
+                    new Vector2(min, max),
+                    new Vector2(otherMin, otherMax)
+                };
+            }
+
+            var points = new[] {min, max, otherMin, otherMax}.OrderBy(x => x).ToArray();
+            return new Vector2[]
+                {
+                    new(points[0], points[1]),
+                    new(points[1], points[2]),
+                    new(points[2], points[3])
+                }
+                .Where(v => Math.Abs(v.X - v.Y) > 0)
+                .Distinct()
+                .ToArray();
+        }
+
+        public override string ToString()
+        {
+            return $"<<{minX}, {minY}, {minZ}>, <{maxX}, {maxY}, {maxZ}>>";
+        }
+
+        protected bool Equals(Region other)
+        {
+            return minX.Equals(other.minX) && maxX.Equals(other.maxX) && minY.Equals(other.minY) && maxY.Equals(other.maxY) && minZ.Equals(other.minZ) && maxZ.Equals(other.maxZ);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Region) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(minX, maxX, minY, maxY, minZ, maxZ);
         }
     }
 }
